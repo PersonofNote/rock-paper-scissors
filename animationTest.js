1bf0d663fc5c05c03995dc249bdf4884e036ec8a
PersonofNote <jtm89@nau.edu>
2018-02-10 18:04:44 -0800

    Improving front end; added text display of round and overall winner, as well as tally marks for scores. Working on getting animation running.

--- animationTest.js
+++ animationTest.js
@@ -1,48 +1,75 @@
-var coinImage = new Image();
-coinImage.src = "coin-test.png";
+var coinImage = new Image(); //Create new image
+coinImage.src = "coin-test.png"; //Set that image's source
 
-function sprite (options) {
-	var that = {};
-
-	that.context = options.context;
-	that.width = options.width;
-	that.height = options.height;
-	that.image = options.image;
-
-that.render = function () {
-
-        // Draw the animation
-        that.context.drawImage(
-           that.image,
-           0,
-           0,
-           that.width,
-           that.height,
-           0,
-           0,
-           that.width,
-           that.height);
-        console.log("render has run")
-    };
-
-    return that;    
-}
-
-
-
-var canvas = document.getElementById("coinAnimation");
+var canvas = document.getElementById("coinAnimation"); //Set the desired canvas to a var, and also set its size while you're at it.
 canvas.width = 42;
 canvas.height = 42;
 
+
+//Note that "options" is an unspecified number of parameters, all of which are optional.
+function sprite (options) {
+  //create an empty object set to the var "that."
+	var that = {},
+      frameIndex = 0,
+        tickCount = 0,
+        ticksPerFrame = options.ticksPerFrame || 0,
+        numberOfFrames = options.numberOfFrames || 1;
+
+    //This is where the options are, but, like...?    
+    that.context = options.context;
+    that.width = options.width;
+    that.height = options.height;
+    that.image = options.image;
+
+that.render = function () {
+        // Draw the animation
+        that.context.drawImage(
+           that.image,
+           frameIndex * that.width / numberOfFrames,
+           0,
+           that.width/numberOfFrames,
+           that.height,
+           0,
+           0,
+           that.width/numberOfFrames,
+           that.height)
+        console.log("Render has run.")
+    };
+
+       that.update = function () {
+
+        tickCount += 1;
+        console.log("Updated.")
+      
+        if (tickCount > ticksPerFrame) {
+        
+          tickCount = 0;
+          
+            if (frameIndex < numberOfFrames - 1) {
+            // Go to the next frame
+            frameIndex += 1; 
+          }
+        }
+    }; 
+    console.log(that + " is being called now."); 
+    console.log(that.context + " is the context.");
+    console.log("Image = " + that.image);
+    return that;
+       
+}
+
+//make a coin by calling the sprite function and passing it the context, width, height and src as arguments.
 var coin = sprite ({
-	context: canvas.getContext("2d"),
-	width: 42,
-	height: 42,
-	image: coinImage
+  context: canvas.getContext("2d"),
+  width: 42,
+  height: 42,
+  image: coinImage
 })
+
 
 
 console.log("AnimationTest has loaded.");
 
 coin.render();
+//coin.update();
 //Is it just rendering once? What's happening here?
--- game.js
+++ game.js
@@ -2,78 +2,111 @@
 //When all you've got is an 'if' statement, everything looks like a conditional! Or something.
 
 
+var playerHaschosen = false; //This is supposed to help keep track of the rounds, but I think I don't need it.
+var playerSelection;
+var computerSelection;
+let roundsPlayed = 0;
+let playerScore = 0;
+let computerScore = 0;
+
+document.getElementById("playAgain").style.display = "none";
+//document.querySelector("#displayScore").innerHTML = ("Player score: " + playerScore + ", Computer score: " + computerScore);
+
+function reply_click(clicked_id)
+			{
+                playerSelection = clicked_id;
+                playGame();
+
+            }
+
+
+
+
 
 function computerPlay()
   {
-   let choice = ["scissors","rock", "paper"];
-   let computerChoice = choice[Math.floor(Math.random() * choice.length)];
-   return computerChoice
+   var choice = ["scissors","rock", "paper"];
+   var computerSelection = choice[Math.floor(Math.random() * choice.length)];
+   return computerSelection;
+   console.log("Computer chose: " + computerSelection);
   }
 
 
 function playRound(playerSelection, computerSelection) {
-	var rock, paper, scissors
-	rock = "rock"
-	paper = "paper"
-	scissors = "scissors"
+	var rock, paper, scissors, playerWin, playerLose, playerTie, winner;
+	rock = "rock";
+	paper = "paper";
+	scissors = "scissors";
+	roundsPlayed ++;
+	//var output = function compareSelections() {
 	if (playerSelection == "rock")
 	  {
 	    if (computerSelection == "rock")
 	      {
-	        return "tie"
+	        return "playerTie";
+	        //console.log("tie");
 	      }
 	    if (computerSelection == "paper")
 	      {
-	        return "playerLose"
+	        return "playerLose";
+	        //console.log("playerLose")
 	      }
 	    if (computerSelection == "scissors")
 	      {
-	        return "playerWin"
+	        return "playerWin";
+	       // console.log("playerWin")
 	      }
 	  }
 	 else if (playerSelection == "paper")
 	 {
 	   	    if (computerSelection == "rock")
 	      {
-	        return "playerWin"
+	        return "playerWin";
+	        //console.log("playerWin");
 	      }
 	    if (computerSelection == "paper")
 	      {
-	        return "tie"
+	        return "playerTie";
+	        //console.log("tie");
 	      }
 	    if (computerSelection == "scissors")
 	      {
-	        return "playerLose"
+	        return "playerLose";
+	        //console.log("playerLose");
 	      }
 	 }
 	 else if (playerSelection == "scissors")
 	 {
 	   	    if (computerSelection == "rock")
 	      {
-	        return "playerLose"
+	        return "playerLose";
 	      }
 	    if (computerSelection == "paper")
 	      {
-	        return "playerWin"
+	        return "playerWin";
 	      }
 	    if (computerSelection == "scissors")
 	      {
-	        return "tie"
+	        return "playerTie";
 	      }
 	 }
-	 
-	 else
+	 else 
 	 {
-	   return "That's against the rules!"
+	   console.log( "Something went wrong... I don't even know how you got this message. Good job?")
 	 }
-
+	//}
+	//return output;
+	//console.log("Round Output = " + output);
 }
 
-function playerChoose()
+
+
+
+/*function playerChoose()
 	{
 		var choice = prompt("Choose your weapon!");
 
-		if (choice.toLowerCase != "rock" && choice.toLowerCase != "paper" && choice.toLowerCase != "scissors")
+		if (choice.toLowerCase() != "paper") 
 			{
 			alert("Hey, no cheating.");
 			playerChoose();
@@ -81,16 +114,109 @@ function playerChoose()
 		else
 		{
   			alert("Good choice");
-  			var playerChoice = choice;
+  			return choice;
 		}
 	}
+	*/
+
+
+function game() {
+  if (roundsPlayed < 5)
+    {
+      var outcome = playRound(playerSelection, computerSelection);
+      var tally = document.createElement('div');
+      tally.className="tallyMark";
+      if (outcome == "playerWin")
+        {
+          playerScore += 1;
+          var winner = "You win round " + roundsPlayed; 
+           console.log("Player score + 1")
+           console.log("Rounds played = " + roundsPlayed);
+          console.log("playerScore =" + playerScore);
+           console.log("computerScore = " + computerScore);
+           document.getElementById("playerScoreDisplay").appendChild(tally);
+           
+        }
+      else if (outcome == "playerLose")
+      {
+        computerScore +=1;
+        winner = "Computer wins round " + roundsPlayed;
+        console.log("Computer score + 1")
+       console.log("playerScore = " + playerScore);
+       console.log("computerScore = " + computerScore);
+       console.log("Rounds played = " + roundsPlayed);
+       document.getElementById("computerScoreDisplay").appendChild(tally);
+      }
+      else
+      {
+       winner = "Round " + roundsPlayed + "is a tie; everyone loses."
+       console.log("Tie, no score increases.")
+       console.log("playerScore = " + playerScore);
+       console.log("computerScore = " + computerScore);
+       console.log("Rounds played = " + roundsPlayed);
+      }
+      
+       //playerHaschosen = false;
+       document.querySelector("#logMoves").innerHTML = ("Player chose: " + playerSelection + "Computer chose: " + computerSelection + " " + winner);
+       //document.querySelector("#displayScore").innerHTML = ("Player score: " + playerScore + ", Computer score: " + computerScore);
+       console.log("Player selection for round " + roundsPlayed + ": " + playerSelection);
+       console.log("Computer chose: " + computerSelection);
+    }
+   
+   if (roundsPlayed >= 5)
+    {
+      if (playerScore > computerScore)
+      {
+      	var finalScore = "Player wins!";
+        console.log("Player wins!")
+      }
+      else if (playerScore < computerScore)
+      {
+      	var finalScore = "Computer Wins!"
+        console.log("Computer wins!")
+      }
+      else 
+      {
+        console.log("Tie overall.")
+        var finalScore = "It's a draw!"
+      }
+      document.getElementById("playAgain").style.display = "block";
+      document.querySelector("#displayScore").innerHTML = "FINAL RESULT: " + finalScore;
+    }
+}
+
+function playGame() {
+	computerSelection = computerPlay();
+	//playRound(playerChoice,computerSelection);
+	game();
+}
+
+function resetGame() {
+	roundsPlayed = 0;
+	computerScore = 0;
+	playerScore = 0;
+	document.querySelector("#logMoves").innerHTML ="";
+	//document.querySelector("#displayScore").innerHTML = ("Player score: " + playerScore + ", Computer score: " + computerScore);
+	document.querySelector("#displayScore").innerHTML = " ";
+	document.getElementById("playAgain").style.display = "none";
+	 document.getElementById("playerScoreDisplay").innerHTML = " ";
+	  document.getElementById("computerScoreDisplay").innerHTML = " ";
+}
 
 
 
-const playerSelection = playerChoose()
-const computerSelection = computerPlay()
-console.log(computerSelection)
-console.log(playRound(playerSelection, computerSelection))
+//const playerSelection = prompt("Rock, paper, scissors?");
+//console.log(playerSelection);
+//computerSelection = computerPlay();
+//console.log(computerSelection);
+
+//document.getElementsByClassName("choice")[0].addEventListener("click", playGame);
+//document.addEventListener("click", playGame());
+
+
+
+
+//console.log(playRound(playerSelection, computerSelection));
 
 
 
@@ -119,4 +245,10 @@ function computerPlay()
 const playerSelection = 'rock'
 const computerSelection = computerPlay()
 console.log(playRound(playerSelection, computerSelection))
+
+
+
+Get element by class (button class),
+Then get element by id, and set the playerinput appropriately,
+then call game.
 */
